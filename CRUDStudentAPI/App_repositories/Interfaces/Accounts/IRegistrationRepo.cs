cb562067bb9df116d26c70cd0f997843d835aa2e
fremont <fremont.babra@gmail.com>
2022-11-04 16:29:15 -0700

    working

--- CRUDStudentAPI/App_repositories/Interfaces/Accounts/IRegistrationRepo.cs
+++ CRUDStudentAPI/App_repositories/Interfaces/Accounts/IRegistrationRepo.cs
@@ -5,7 +5,10 @@ namespace CRUDStudentAPI.App_repositories.Interfaces.Accounts
     public interface IRegistrationRepo
     {
         Task<Registration> createStudent(Registration registration);
+        string RegisterStudent(Registration registration);
+        bool LoginStudent(Login student);
         Task<Registration> updateStudent(Registration registration);
+        Task<Registration> AccountExists(string studentNo);
 
     }
 }
---
+++ CRUDStudentAPI/Migrations/20221028215545_hello.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDStudentAPI.Migrations
+{
+    public partial class hello : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portal",
+                columns: table => new
+                {
+                    StudentNo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConfirmPassword = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portal", x => x.StudentNo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Portal");
+        }
+    }
+}
